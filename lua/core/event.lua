d011e02df68bef81a0e6db56f4718bd8864085d7
alo2 <tuanlong@wust.edu.cn>
2022-04-08 09:12:36 +0800

    delete vscode config

--- lua/core/event.lua
+++ lua/core/event.lua
@@ -21,7 +21,8 @@ function autocmd.load_autocmds()
 			{
 				"BufWritePost",
 				[[$VIM_PATH/{*.vim,*.yaml,vimrc} nested source $MYVIMRC | redraw]],
-			}, -- Reload Vim script automatically if setlocal autoread
+			},
+      -- Reload Vim script automatically if setlocal autoread
 			{
 				"BufWritePost,FileWritePost",
 				"*.vim",
